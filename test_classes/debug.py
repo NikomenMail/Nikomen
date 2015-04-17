b0ad97def5b40fec0e4474a606a9ca19509e3e02
William Fong <trigition@gmail.com>
2015-04-17 14:21:47 -0600

    Gmail doesn't like underscores, removed them from email addresses

--- test_classes/debug.py
+++ test_classes/debug.py
@@ -3,8 +3,8 @@
 from emailGenerator import EmailGenerator
 
 #Stuff to know
-# Send To: nikomen_test@gmail.com
-# Send From: nikomen_client@gmail.com
+# Send To: nikomenTest@gmail.com
+# Send From: nikomenClient@gmail.com
 generator = EmailGenerator('yolo@swag.org', 'nikomen@wat.com')
 generator.printEmail()
 
