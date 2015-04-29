e1f360bf1f19030cac7f6d28ea4c3f01d5215bdb
William Fong <trigition@gmail.com>
2015-04-28 22:25:25 -0600

    Implemented Email attachments and cc capabilities

--- applications/nikomen/modules/Box/Email.py
+++ applications/nikomen/modules/Box/Email.py
@@ -6,25 +6,27 @@ import datetime
 
 
 class Email(object):
+    """This class represents an email"""
     def __init__(self, sAddr, rAddr, subject, message, attachment):
         self.sAddr = sAddr
         self.rAddr = rAddr
         self.subject = subject
         self.message = message
         self.attachment = attachment
-
         self.time = datetime.datetime.now()
-
         self.isRead = True
 
     def reply(self):
+        """Opens a reply email for replying"""
         Box.write()
 
     def forward(self):
+        """Forwards an email"""
         Box.write()
         pass
 
     def read(self):
+        """Reads an email still requires implmentation"""
         if self.isRead:
             self.isRead = False
         else:
@@ -39,17 +41,19 @@ class Email(object):
         pass
 
     def print_email(self):
+        """Prints an email to standard out"""
         print self.time
         print "   From : " + self.sAddr
         print "   Subject: " + self.subject
 
     @staticmethod
-    def write(sAddr, rAddr, subject, message, attachment):
+    def write(sAddr, rAddr, subject, message, attachment=None):
         """write a new email"""
         return Email(sAddr, rAddr, subject, message, attachment)
 
     @staticmethod
     def send(email):
+        """Sends the email"""
         pass
 
 
